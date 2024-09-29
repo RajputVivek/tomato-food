4dadeb83f296c1447c2164a4e018ad05cde19e14
RajputVivek <dearvivekji@gmail.com>
2024-09-29 04:18:56 +0000

    Added BrowserRouter 3. Created Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
